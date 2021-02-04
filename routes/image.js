93c979e8c81c861f97915452a19e045560de82d5
Ketan <ketankatore9@gmail.com>
2021-02-04 17:11:20 +0530

    feat : delete  image api

--- routes/image.js
+++ routes/image.js
@@ -1,3 +1,4 @@
+const fs = require('fs').promises ;
 const { Router } = require('express');
 const { sendReply, reportError } = require('../utils/response');
 const uploadFile = require('../middleware/multer');
@@ -9,9 +10,23 @@ router.post('/', uploadFile.single('file'), async (req, res) => {
         if (req.file == undefined) {
             return res.status(500).send(sendReply(0, "Please upload a file!"));
         }
-        let imageUrl = `${req.protocol}://${req.headers.host}/${req.file.filename}`;
+        // let imageUrl = `${req.protocol}://${req.headers.host}/${req.file.filename}`;
+        let imageUrl = req.file.filename
         return res.status(200).send(sendReply(1, 'Uploaded', imageUrl));
     } catch (e) {
+        reportError('Error in add new moment', e);
+        return res.status(500).send(sendReply(0, e.message, e));
+    }
+});
+
+router.delete('/', async (req, res) => {
+    try {
+        await fs.unlink('./uploads/'+ req.body.imageUrl);
+        return res.status(200).send(sendReply(1, 'Deleted'));
+    } catch (e) {
+        if(e.code === 'ENOENT'){
+            return res.status(200).send(sendReply(1, 'Already Deleted'));
+        }
         reportError('Error in add new moment', e);
         return res.status(500).send(sendReply(0, e.message, e));
     }
